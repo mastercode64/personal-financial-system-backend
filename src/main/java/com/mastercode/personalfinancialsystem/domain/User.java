a2aa08f45dc2f2a5046a5afa33fe024bd64aa0cd
Marcos Fujihashi <marcosfujihashi@gmail.com>
2019-03-31 16:45:07 -0300

    Adicionado novo campo password ao usuario

--- src/main/java/com/mastercode/personalfinancialsystem/domain/User.java
+++ src/main/java/com/mastercode/personalfinancialsystem/domain/User.java
@@ -1,29 +1,39 @@
 package com.mastercode.personalfinancialsystem.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotBlank
 	private String nome;
+
+	@Email
+	@NotBlank
+	@Column(unique = true)
 	private String email;
-	
-	public User() {		
-	}
-	
-	public User(String nome, String email) {
+
+	@NotBlank
+	private String password;
+
+	public User(String nome, String email, String password) {
 		this.nome = nome;
 		this.email = email;
+		this.password = password;
 	}
-	
-	
 
 }
--- src/main/java/com/mastercode/personalfinancialsystem/dto/UserDTO.java
+++ src/main/java/com/mastercode/personalfinancialsystem/dto/UserDTO.java
@@ -3,25 +3,20 @@ package com.mastercode.personalfinancialsystem.dto;
 import com.mastercode.personalfinancialsystem.domain.User;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDTO {
 
 	private String nome;
 	private String email;
-
-	public UserDTO() {
-	}
-
-	public UserDTO(User user) {
-		this.nome = user.getNome();
-		this.email = user.getEmail();
-	}
+	private String password;
 
 	public User dtoToUser() {
-		return new User(this.nome, this.email);
+		return new User(this.nome, this.email, this.password);
 	}
 
 }
