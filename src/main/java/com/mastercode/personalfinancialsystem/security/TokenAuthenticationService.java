f67a9bacadf95af7a44af5350e0931ffa75e95a4
Marcos Fujihashi <marcosfujihashi@gmail.com>
2019-05-18 17:38:48 -0300

    Changed token expiration time to 30 minutes

--- src/main/java/com/mastercode/personalfinancialsystem/security/TokenAuthenticationService.java
+++ src/main/java/com/mastercode/personalfinancialsystem/security/TokenAuthenticationService.java
@@ -11,8 +11,8 @@ import java.util.Collections;
 import java.util.Date;
 
 public class TokenAuthenticationService {
-    // EXPIRATION_TIME = 10 dias
-    static final long EXPIRATION_TIME = 860_000_000;
+    // EXPIRATION_TIME = 30 minutes
+    static final long EXPIRATION_TIME = 30 * 60 * 1000;
     static final String SECRET = "MySecret";
     static final String TOKEN_PREFIX = "Bearer";
     static final String HEADER_STRING = "Authorization";
