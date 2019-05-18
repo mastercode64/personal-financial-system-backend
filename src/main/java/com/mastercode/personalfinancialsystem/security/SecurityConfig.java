66ab88234a3bda1f377b04fff1ff3443c8d010ed
Marcos Fujihashi <marcosfujihashi@gmail.com>
2019-05-18 19:11:11 -0300

    Added CORS configurations

--- src/main/java/com/mastercode/personalfinancialsystem/security/SecurityConfig.java
+++ src/main/java/com/mastercode/personalfinancialsystem/security/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -35,32 +40,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public AuthenticationEntryPoint entryPoint(){
+    public AuthenticationEntryPoint entryPoint() {
         return new RestAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-        .authorizeRequests()
-            .antMatchers("/home").permitAll()
-            .antMatchers("/login").permitAll()
-            .antMatchers(HttpMethod.POST, "/users").permitAll()
-            .antMatchers("/**").authenticated()
-            .and()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
 
-        .exceptionHandling().authenticationEntryPoint(this.entryPoint()).and()
+                .antMatchers("/home").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .antMatchers("/**").authenticated().and()
 
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling().authenticationEntryPoint(this.entryPoint()).and()
 
-        // filtra requisições de login
-        .addFilterBefore(new JwtLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 
-        // filtra outras requisições para verificar a presença do JWT no header
-        .addFilterBefore(new JwtAuthenticationFilter(),UsernamePasswordAuthenticationFilter.class);
+                // filtra requisições de login
+                .addFilterBefore(new JwtLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
 
+                // filtra outras requisições para verificar a presença do JWT no header
+                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
-
-
 }
